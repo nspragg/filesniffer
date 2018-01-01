4f6c688c90d020d16f0d181a02f8ba454f7574ab
nick spragg <5319732+nspragg@users.noreply.github.com>
2018-01-01 19:41:45 +0000

    Task: Add dist to gitignore.

--- dist/src/NoopCollector.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class NoopCollector {
-    collect() { }
-    matches() { return []; }
-}
-exports.NoopCollector = NoopCollector;
-//# sourceMappingURL=NoopCollector.js.map
